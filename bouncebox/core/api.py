44c16d4bc9c9f6ec0d268c55baa4e4f35b7a6736
Dale Jung <dale@dalejung.com>
2013-02-20 20:00:41 -0500

    expose mixins

--- bouncebox/core/api.py
+++ bouncebox/core/api.py
@@ -1,4 +1,5 @@
 from bouncebox.core.box import *
+from bouncebox.core.mixins import *
 from bouncebox.core.event import *
 from bouncebox.core.component import *
 from bouncebox.core.series import *
