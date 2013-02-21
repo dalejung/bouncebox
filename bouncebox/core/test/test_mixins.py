47ac1411eb87fa35c670fad61e1d05e483ee714c
Dale Jung <dale@dalejung.com>
2013-02-20 22:04:54 -0500

    Moved the MixinBase to be the old Component. I need to re-org more if I want to cleanly add Mixins to the BaseComponent. The BubbleDownMixin relies on things found Component and not just BaseComponent

---
+++ bouncebox/core/test/test_mixins.py
@@ -0,0 +1,42 @@
+import sys
+from unittest import TestCase
+
+from mock import MagicMock
+
+import bouncebox.core.component as bc
+import bouncebox.core.event as be
+import bouncebox.core.mixins as mixins
+
+class TestBase(bc.PreMixComponent):
+    pass
+
+mixins.component_mixin(TestBase, mixins.BubbleDownMixin)
+
+class TestBubbleDownMixin(TestCase):
+
+    def __init__(self, *args, **kwargs):
+        TestCase.__init__(self, *args, **kwargs)
+
+    def runTest(self):
+        pass
+
+    def setUp(self):
+        pass
+
+    def test_bubble_down(self):
+        parent = TestBase()
+        mid = TestBase()
+        child = TestBase()
+
+        child.add_event_listener(be.Event, 'handle_event')
+        child.handle_event = MagicMock()
+
+        # We have an order issue
+        parent.add_component(mid)
+        mid.add_component(child, contained=True)
+        mid.enable_bubble_down(child)
+
+        evt = be.Event()
+        parent.broadcast(evt)
+        # middle ware should have progated events to child
+        child.handle_event.assert_called_once_with(evt)
